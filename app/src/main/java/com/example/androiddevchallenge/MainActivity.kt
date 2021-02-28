b4c47b6397ff9460541346f44ba5f889468c430e
JoshuaChang <joshuachang0823@gmail.com>
2021-03-01 03:11:51 +0800

    Grid

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,17 +15,37 @@
  */
 package com.example.androiddevchallenge
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.palette.graphics.Palette
+import com.example.androiddevchallenge.data.DemoDataProvider
+import com.example.androiddevchallenge.data.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
+
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,13 +57,103 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Start building your app here!
+@ExperimentalFoundationApi
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    Surface(color = MaterialTheme.colors.onBackground) {
+        HomePageList()
     }
 }
 
+@ExperimentalFoundationApi
+@Composable
+fun HomePageList() {
+    val list = remember { DemoDataProvider.puppyList }
+    val bannerList = remember { DemoDataProvider.banner }
+    val bannerLstState = rememberLazyListState()
+
+    LazyVerticalGrid(cells = GridCells.Fixed(2)){
+        items(list.size) { index ->
+            val puppy = list[index]
+            GridItemView(puppy = puppy) {}
+        }
+    }
+}
+
+@Composable
+private fun GridItemView(
+    puppy: Puppy,
+    modifier: Modifier = Modifier,
+    onClick: (puppy: Puppy) -> Unit
+) {
+    val image = ImageBitmap.imageResource(puppy.images.first())
+    val bitmap = image.asAndroidBitmap()
+    val paletteColor = createPaletteSync(bitmap).getVibrantColor(0xBB86FC)
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier
+            .padding(4.dp)
+        ,/*elevation = 5.dp,*/
+        /*backgroundColor = (Color(paletteColor).copy(alpha = 0.2f))*/
+    ) {
+        Box(modifier = Modifier.clickable { onClick.invoke(puppy) }) {
+            Row (modifier = Modifier.padding(top = 4.dp)){
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = puppy.name,
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.padding(1.dp))
+                Image(
+                    modifier = Modifier
+                        .size(16.dp)
+                        .align(Alignment.CenterVertically),
+                    colorFilter = ColorFilter.tint(puppy.gender.color),
+                    imageVector = puppy.gender.label, contentDescription = null
+                )
+                Spacer(modifier = Modifier.weight(1f))
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .fillMaxWidth(0.5f)
+                    .height(240.dp),
+            ) {
+
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = puppy.breed,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.body2
+                )
+                Text(
+                    text = puppy.location,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+            Image(
+                bitmap = image,
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 30.dp,start = 6.dp,end = 6.dp)
+                    .heightIn(max = 180.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(6.dp))
+            )
+        }
+    }
+}
+
+// Generate palette synchronously and return it
+fun createPaletteSync(bitmap: Bitmap): Palette = Palette.from(bitmap).generate()
+
+
+@ExperimentalFoundationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
@@ -52,6 +162,7 @@ fun LightPreview() {
     }
 }
 
+@ExperimentalFoundationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
